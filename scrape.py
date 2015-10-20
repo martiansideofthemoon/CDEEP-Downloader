616cf0004871e493782284fc1af36d1ec96b40cd
KALPESH KRISHNA <kalpeshk2011@gmail.com>
2015-10-21 01:10:00 +0530

    added progress bar, robust find courses

--- scrape.py
+++ scrape.py
@@ -1,4 +1,4 @@
-import os,re,subprocess
+import os,re,subprocess,itertools,time,sys
 
 def validate_input(course):
 	courseRegex = re.compile(r'^([A-Z][A-Z])\s?(\d\d\d)$',re.VERBOSE)
@@ -8,13 +8,51 @@ def validate_input(course):
 	else:
 		return result.group(1)+result.group(2)
 
+def print_progress(count):
+	sys.stdout.flush()
+	percent = count*100/112
+	progress = "Running..."+str(percent)+"% "
+	print "\r"+progress,
+
 course = raw_input("Please enter your course :- ")
 course = validate_input(course)
 
 # 'A' represents Autumn and 'S' represents Spring
-type1 = ['A','S']
-year = ['02','03','04','05','06','07','08','09','10','11','12','13','14','15']
-settings = []
+semester = ['A','S']
+# Courses on CDEEP vary from 2002 to 2015
+year = ['02','03','04','05','06','07','08',
+		'09','10','11','12','13','14','15']
+# Filetype added for two possible course files
+filetype = ['mp4','flv']
+# Type of file (complete or part A to look for during brute force
+brute_types = ['','-A']
+base_url = "rtmp://epay.cdeep.iitb.ac.in:1935/vod/Videos/"
+courses_found = []
+count=1
+for i,j,k,l in itertools.product(brute_types,filetype,semester,year):
+	url = base_url+k+l+"/"+course+"/"+course+"-L1"+i+"."+j
+	command = ["rtmpdump","-r "+url,"-o test."+j]
+
+	print_progress(count)
+	count+=1
+	start=time.clock()
+	process = subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	while (process.poll()==None):
+		if (time.clock()-start>1):
+			process.kill()
+			courses_found.append({"semester":k,"year":l,"filetype":j});
+			break
+
+print "\nWe found the following courses. Choose one. "
+num = 1
+for course in courses_found:
+	if (course["semester"]=="S"):
+		print str(num)+". Spring 20"+course["year"]
+	else:
+		print str(num)+". Autumn 20"+course["year"]
+	num+=1
+
+"""
 for m in ['','-A']:
 	for k in ['mp4','flv']:
 		for i in type1:
@@ -22,7 +60,7 @@ for m in ['','-A']:
 				if (os.system("rtmpdump -r rtmp://epay.cdeep.iitb.ac.in:1935/vod/Videos/"+i+j+"/"+course+"/"+course+"-L1"+m+"."+k+" -o civil."+k))==256:
 					continue
 				else:
-					settings.append({"semester":i,"year":j,"filetype":k});
+					
 print "The following courses were found :- "					
 for i in settings:
 	print i["semester"]+" "+i["year"]
@@ -39,4 +77,4 @@ for i in range(1,50):
 			continue;
 		downloaded.append({"number":i,"type":j});
 print downloaded
-
+"""
